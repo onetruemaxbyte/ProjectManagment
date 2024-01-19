d6c220fc1bee1862593c4ad7c6e80d78ed64ca23
Eldos Yksanov <eldosyksanov@gmail.com>
2024-01-19 21:54:05 +0300

    added 3 EntityFrameworkCore libraries

--- ProjectManagment/Models/Project.cs
+++ ProjectManagment/Models/Project.cs
@@ -2,12 +2,14 @@
 {
     public class Project
     {
-       private string ProjectName {get; set;}
-       private string CustomerCompany { get; set; }
-       private string ExecutorCompany { get; set; }
-       private DateTime StartDate { get; set; }
-       private DateTime EndDate { get; set; }
-       private int Priority { get; set; }
+        private string ProjectName { get; set; }
+        private string CustomerCompany { get; set; }
+        private string ExecutorCompany { get; set; }
+        private Employee Employee { get; set; }
+        private Employee Manager { get; set; }
+        private DateTime StartDate { get; set; }
+        private DateTime EndDate { get; set; }
+        private int Priority { get; set; }
 
     }
 }
