6d2e6c14a52deed15ac61ef51250af1bf4fdceb0
Eldos Yksanov <eldosyksanov@gmail.com>
2024-01-19 21:35:27 +0300

    added

---
+++ ProjectManagment/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectManagment.Models
+{
+    public class Employee
+    {
+        private string EmployeeName { get; set; }
+        private string EmployeeFirstName { get; set; }
+        private string EmployeeLastName { get; set; }
+        private string EmployeeEmail { get; set; }
+
+    }
+}
---
+++ ProjectManagment/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectManagment.Models
+{
+    public class Project
+    {
+       private string ProjectName {get; set;}
+       private string CustomerCompany { get; set; }
+       private string ExecutorCompany { get; set; }
+       private DateTime StartDate { get; set; }
+       private DateTime EndDate { get; set; }
+       private int Priority { get; set; }
+
+    }
+}
